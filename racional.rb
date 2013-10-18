f9e41a3d205012cfa5102c06a82a961a804af640
Rushil Lakhani Lakhani <alu4093@cc404.(none)>
2013-10-18 15:53:52 +0100

    Añadiendo_metodo_racional terminado

--- racional.rb
+++ racional.rb
@@ -3,36 +3,74 @@
 require "./gcd.rb"
 
 # define la clase Fraccion
+
 class Fraccion
 
 # método inicializar clase
+  
     def initialize (num,denom)
-    # atributos
+      # atributos      
       @num = num;
       @denom = denom; 
     end
 
- # convertir a una cadena
+# convertir a una cadena
+    
     def to_s 
 	"#{@num}/#{@denom}"
-   end
+    end
   
-   def suma
+    def suma(a,b) # suma de fracciones
+      "#{@num}/#{@denom} + #{a}/#{b} ="
+      aux = (@denom/gcd(@denom,b)) * b # Calculo del mcm usando la propiedad (a*b/mcd(a,b))
+      aux2 = ((aux/@denom) * @num) + ((aux/b) * a) # Calculo de la suma
+      # Mostrar por pantalla resultado     
+      if aux2 % aux == 0 # Compruebo si la divison es exacta para imprimir el resultado de la division
+	 aux3 = aux2/aux
+      else
+         "#{aux2}/#{aux}"
+      end  
+    end
    
-   end
    
-   def producto
-     
-   end
+    def resta(a,b) #resta de fracciones
+      "#{@num}/#{@denom} + #{a}/#{b} ="
+      aux = (@denom/gcd(@denom,b)) * b # Calculo del mcm usando la propiedad (a*b/mcd(a,b)) 
+      aux2 = ((aux/@denom) * @num) - ((aux/b) * a) # Calculo de la resta
+      # Mostrar por pantalla el resultado
+      if aux2 % aux == 0 # Compruebo si la divison es exacta para imprimir el resultado de la division
+	 aux3 = aux2/aux
+         "#{aux3}"
+      else
+         "#{aux2}/#{aux}"
+      end
+    end   
    
-   def resta
-     
-   end
-   
-   def division
-     
-     
-   end
+    
+    def producto(a,b) # Producto de fracciones
+      "#{@num}/#{@denom} * #{a}/#{b} ="
+      # Calculo del producto
+      aux = @denom * b
+      aux2 = @num * a
+      # Mostrar por pantalla resultado
+      if aux2 % aux == 0 # Compruebo si la divison es exacta para imprimir el resultado de la division
+         aux2/aux
+      else
+         "#{aux2}/#{aux}"
+      end
+    end
+        
+    def division(a,b) # Division de fracciones
+      "#{@num}/#{@denom} / #{a}/#{b} ="
+       # Calculo del producto
+       aux = @denom * a
+       aux2 = @num * b
+       # Mostrar por pantalla resultado
+       if aux2 % aux == 0 # Compruebo si la divison es exacta para imprimir el resultado de la division
+          aux2/aux
+       else
+          "#{aux2}/#{aux}"
+       end
+     end
 end  
-   f = Fraccion.new(12,3)
-   puts f.to_s
+
